801770b3dfa61b679f396ee21e086f91b69c66f7
Aref Moosavi <Aref.Moosavi7813@gmail.com>
2026-01-01 10:53:51 +0330

    changeable models

--- lib/view/mainscreen.dart
+++ lib/view/mainscreen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/product.dart';
 import 'package:get/get.dart';
 
 class Mainscreen extends StatelessWidget {
   var counter = 0.obs;
+  Rx<Product> product = Product(name: "chips", price: 10000, offer: 2).obs;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,16 +15,20 @@ class Mainscreen extends StatelessWidget {
             Obx(() {
               return Column(
                 children: [
-                  Text("product name : $counter"),
-                  Text("product price : $counter"),
-                  Text("product offer : $counter"),
+                  Text("product name : ${product.value.name}"),
+                  Text("product price : ${product.value.price}"),
+                  Text("product offer : ${product.value.offer}"),
                 ],
               );
             }),
             SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
-                counter = counter + 1;
+                product.update((val) {
+                  val!.name = "pofak";
+                  val.price = 8000;
+                  val.offer = 0;
+                });
               },
               child: Text("Press"),
             ),
